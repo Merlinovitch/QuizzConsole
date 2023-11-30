01d546670fec412cc6903ee8825edc8562d2347b
Merlinovitch <a.merlin92@gmail.com>
2023-11-30 17:20:51 +0100

    fix: :bug: fix DeleteQuestion bug

--- DeleteQuestion.cs
+++ DeleteQuestion.cs
@@ -16,46 +16,47 @@ namespace QuizzConsole
             {
                 Console.WriteLine("Voulez vous supprimer une question ? (o/n)");
                 string? choixUser = Console.ReadLine();
-                if (choixUser != "o" || choixUser != "O" || choixUser != "n" || choixUser != "N")
+                if (choixUser != "o" && choixUser != "O" && choixUser != "n" && choixUser != "N")
                 {
                     Console.WriteLine("Le choix doit etre o ou n");
                     SuppQuestion();
-                    if (choixUser == "o" || choixUser == "O")
+                }
+                else if (choixUser == "o" || choixUser == "O")
+                {
+                    string filePath = questionLoader.getFilePath();
+                    string[] lignes = File.ReadAllLines(filePath);
+                    List<string[]> questions = lignes.Select(l => l.Split(";")).ToList();
+
+                    for (int i = 0; i < questions.Count; i++)
                     {
-                        string filePath = questionLoader.getFilePath();
-                        string[] lignes = File.ReadAllLines(filePath);
-                        List<string[]> questions = lignes.Select(l => l.Split(";")).ToList();
-
-                        for (int i = 0; i < questions.Count; i++)
-                        {
-                            Console.WriteLine(i + 1 + ". " + questions[i][0]);
-                        }
-                        Console.WriteLine("Entrez le numéro de la question à supprimer :");
-                        int questionASupp = int.Parse(Console.ReadLine());
-                        lignes = lignes.Where((line, index) => (index + 1) != questionASupp).ToArray();
-                        File.WriteAllLines(filePath, lignes);
-                        Console.WriteLine($"La question " + questionASupp + " a bien été supprimée");
-
-                        Console.WriteLine("Voulez vous en supprimer une autre ? (o/n)");
-                        string? suppAgain = Console.ReadLine();
-
-                        if (suppAgain == "o" || suppAgain == "O")
-                        {
-                            SuppQuestion();
-                        }
-                        else if (choixUser == "n" || choixUser == "N")
-                        {
-
-                        }
-
+                        Console.WriteLine(i + 1 + ". " + questions[i][0]);
                     }
-                    else
+                    Console.WriteLine("Entrez le numéro de la question à supprimer :");
+                    int questionASupp = int.Parse(Console.ReadLine());
+                    lignes = lignes.Where((line, index) => (index + 1) != questionASupp).ToArray();
+                    File.WriteAllLines(filePath, lignes);
+                    Console.WriteLine($"La question " + questionASupp + " a bien été supprimée");
+
+                    Console.WriteLine("Voulez vous en supprimer une autre ? (o/n)");
+                    string? suppAgain = Console.ReadLine();
+
+                    if (suppAgain == "o" || suppAgain == "O")
+                    {
+                        SuppQuestion();
+                    }
+                    else if (choixUser == "n" || choixUser == "N")
                     {
 
                     }
+
+                }
+                else
+                {
+
                 }
             }
         }
-
     }
+
 }
+
